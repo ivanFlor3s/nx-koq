7db8689ca234a8e487a6fe09b0cefa20747b8e4d
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-07-09 01:18:28 -0300

    npx prisma migrate dev + generate

---
+++ apps/api/prisma/migrations/20220709040915_/migration.sql
@@ -0,0 +1,31 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "googleAcount" BOOLEAN NOT NULL DEFAULT false,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "lastname" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "hash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
