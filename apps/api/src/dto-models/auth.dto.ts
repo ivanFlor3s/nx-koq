99632c711b4bf5ba95fa94a7719e7f44b9c4f634
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-07-09 14:25:52 -0300

    Agrego interfaces

---
+++ apps/api/src/dto-models/auth.dto.ts
@@ -0,0 +1,22 @@
+import { AuthDto as AuthInterface } from "@nx-koq/api-interfaces";
+import {IsBoolean, IsEmail, IsNotEmpty, IsString, IsUrl} from 'class-validator'
+
+export class AuthDto implements AuthInterface{
+
+    @IsString()
+    @IsUrl()
+    imageUrl: string;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    googleAcount: boolean;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+}
---
+++ libs/api-interfaces/src/lib/models/auth/auth.dto.ts
@@ -0,0 +1,8 @@
+export interface AuthDto {
+    email:string
+    // name:string,
+    // lastname:string
+    imageUrl: string,
+    googleAcount:boolean
+    password: string
+}
