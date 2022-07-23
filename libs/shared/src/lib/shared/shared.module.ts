3a02edc5bef72fb27a28ee8459a240b0dd8bbead
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-07-23 19:11:29 -0300

    Shared lib

---
+++ libs/shared/src/lib/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [],
+  exports:[
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
